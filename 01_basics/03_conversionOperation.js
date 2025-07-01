2ef4e3f70274d9d70935582428f85e178c766a7e
Tajamul <tajamulajaz4@gmail.com>
2025-07-01 06:36:00 +0000

    Conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,32 @@
+let score = "undefined"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+//  "33" => 33 //
+//  "33abc" => NaN ( full form "Not A Number" ) //
+//  ture => 1; false => 0 //
+//  null => 0; //
+//  undefined => NaN //
+
+
+let isLoggedIn = 1
+
+let BooleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(BooleanIsLoggedIn);
+
+//  1 => true; 0 => false //
+//  "" => false //
+//  "tajamul" => true //
+
+let someNumber = 33
+
+let stringNumber = String (someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
